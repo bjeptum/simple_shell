d8c47d227069709ba7a79300b98791e209cfc2be
b_jeptum <brendajeptum@gmail.com>
2022-11-14 04:23:37 -0800

    Get pid and ppid

---
+++ pid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - get pid and ppid
+*
+* Return: Always 0
+*/
+int main(void)
+{
+	int p_id, p_pid;
+
+	p_id = getpid(); /*process id*/
+	p_pid = getppid(); /*parent process id*/
+
+	printf("Process ID: %d\n", p_id);
+	printf("Parent Process ID: %d\n", p_pid);
+
+	return (0);
+}
