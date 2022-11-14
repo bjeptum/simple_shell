b06eb62b1171812b14ae2db8bd5da82e34f42dbe
faithkangui <faithkangui@gmail.com>
2022-11-14 12:43:25 +0300

    prompt file

---
+++ prompt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(__attribute__((unused))int argc, __attribute__((unused)) char **argv)
+{
+	size_t len = 0;
+	char *line = NULL;
+	ssize_t nread;
+
+	print("$ ");
+
+	nread = getline(&line, &len, stdin);
+	printf("%s", line);
+	printf("Number of characters: %ld/n", nread);
+
+	free(line);
+	exit(EXIT SUCCESS);
+}
