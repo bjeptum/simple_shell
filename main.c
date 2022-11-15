73d2b97ef050990edf10d0b5ef27fbef8e1493d4
b_jeptum <brendajeptum@gmail.com>
2022-11-15 04:25:27 -0800

    Read input and parse

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include "shell.h"
+#include <unistd.h>
+#include <sys/wait.h>
 /**
 * readline: reads an input into the buffer
 *
@@ -43,4 +45,42 @@ char *readline()
 			}
 		}
 	}
+}
+/**
+* splitline - breaks a line into tokens
+* @line: line to be splitted
+*
+* Return: tokens (Abstract syntax Tree)
+*/
+char **splitline(char *line)
+{
+	char **tokens = (char **)malloc(sizeof(char *) * 64);
+	const char *token;
+	char delim[10] = "\t\n\r\a";
+	int ps = 0, bufsize = 64;
+
+	if (!tokens)
+	{
+		printf("\nBuffer Allocation Error.");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(line, delim);
+	while (token != NULL)
+	{
+		token[ps] = token;
+		ps++;
+		if (ps >= bufsize)
+		{
+			bufsize += 64;
+			line = realloc(line, bufsize * sizeof(char *));
+			if (!line)
+			{
+				printf("\nBuffer Allocation Error.");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, delim);
+	}
+	token[ps] = NULL;
+	return (tokens);
 }
